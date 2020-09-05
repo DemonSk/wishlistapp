db5a38c798560c5c076671fcbe6c7c0d869a0736
DemonSk <venna2013@gmail.com>
2020-09-05 19:49:48 +0300

    Added sqlite to test deploy

--- wishapp/settings.py
+++ wishapp/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'wishapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,7 +108,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

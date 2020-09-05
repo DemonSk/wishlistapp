acea10b98569140ed772bbe63f47135c66de5628
DemonSk <venna2013@gmail.com>
2020-09-05 19:05:22 +0300

    Added Procfile

--- wishapp/settings.py
+++ wishapp/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'wishlist-home'
 LOGIN_URL = 'login'

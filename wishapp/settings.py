0e45d051461164fd7b8ca02b5c13f20688dc61d9
DemonSk <venna2013@gmail.com>
2020-09-05 18:48:25 +0300

    Added stuff for heroku

--- wishapp/settings.py
+++ wishapp/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = 'wishlist-home'
 LOGIN_URL = 'login'

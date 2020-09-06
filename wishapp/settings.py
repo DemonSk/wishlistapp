555a61c8051da162cbfcff2000ea9456684c9c28
DemonSk <venna2013@gmail.com>
2020-09-06 10:31:00 +0300

    Added database conn

--- wishapp/settings.py
+++ wishapp/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'wishapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -108,14 +108,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
